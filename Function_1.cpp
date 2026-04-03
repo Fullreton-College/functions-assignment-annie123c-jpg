fd0976d5e2d1e1d1ecabdae74e478a20495b45d9
Chapman, Anne <annie123c@gmail.com>
2026-04-03 13:01:17 -0700

    Code Written

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 /* Define your function here */ 
+string CoinFlip()
+{
+   int result = rand() % 2;
+
+   if (result == 1)
+      return "Heads";
+   else
+      return "Tails";
+}
 
 int main() {
    // Add more variables as needed
@@ -10,6 +19,14 @@ int main() {
    srand(2);  // Unique seed
 
    /* Type your code here */
+   int numFlips;
+   cout << "Enter the number of times to flip the coin: ";
+   cin >> numFlips;
+
+   for (int i = 0; i < numFlips; i++) 
+   {
+      cout << CoinFlip() << endl;
+   }
 
    return 0;
 }
