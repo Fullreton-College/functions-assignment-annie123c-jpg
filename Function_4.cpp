a3854b0f4d5cac4dca3de6ce25f2a926358f0e74
Chapman, Anne <annie123c@gmail.com>
2026-04-03 16:09:15 -0700

    Code Written

--- Function_4.cpp
+++ Function_4.cpp
@@ -2,12 +2,47 @@
 using namespace std;
 
 //define necessary functions
+void computeCoins (int coinValue, int& num, int& amountLeft);
+
 int main()
 {
+  int amount;
+  char choice;
 
-  //your code goes here
+  do
+  {
+    cout << "Enter amount (1-99): ";
+    cin >> amount;
+
+    if (amount < 1 || amount > 99)
+    {
+      cout << "Invalid input.\n";
+      continue;
+    }
+
+    int amountLeft = amount;
+    int quarters, dimes, pennies;
+
+    computeCoins(25, quarters, amountLeft);
+    computeCoins(10, dimes, amountLeft);
+    computeCoins(1, pennies, amountLeft);
+
+    cout << amount << " cents can be given as ";
+    cout << quarters << " quarter(s), ";
+    cout << dimes << " dime(s), and ";
+    cout << pennies << " penny(pennies).\n";
+
+    cout << "Do you want to enter another amount? (y/n): ";
+    cin >> choice;
+    
+  } while (choice == 'y' || choice == 'Y');
 
 return 0;
 }
 
 //implement functions
+void computeCoins (int coinValue, int& num, int& amountLeft)
+{
+  num = amountLeft / coinValue;
+  amountLeft = amountLeft % coinValue;
+}
